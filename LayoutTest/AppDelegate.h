6c0533d25f6de630265cb014bf84e48efc5775b0
chebao <chebao@chebaodeimac.local>
2016-04-05 10:11:32 +0800

    Initial Commit

---
+++ LayoutTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LayoutTest
+//
+//  Created by chebao on 16/4/5.
+//  Copyright © 2016年 chebao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LayoutTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LayoutTest
+//
+//  Created by chebao on 16/4/5.
+//  Copyright © 2016年 chebao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
